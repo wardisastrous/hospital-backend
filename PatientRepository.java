71120945cdccdbbd82e4afab04f43b0ff0ceabbe
Uday Kapila <udaykapila@Udays-MacBook.local>
2026-04-12 16:19:20 +0530

    neon db added

---
+++ PatientRepository.java
@@ -0,0 +1,9 @@
+package com.uday.hospitalmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.uday.hospitalmanagement.entity.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
