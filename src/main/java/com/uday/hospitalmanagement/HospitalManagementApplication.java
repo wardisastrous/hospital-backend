d7914eb115778e5c4731469650dcd879fb2810fb
Uday Kapila <udaykapila@Udays-MacBook.local>
2026-04-12 17:10:09 +0530

    fix repository bean detection

--- src/main/java/com/uday/hospitalmanagement/HospitalManagementApplication.java
+++ src/main/java/com/uday/hospitalmanagement/HospitalManagementApplication.java
@@ -2,12 +2,15 @@ package com.uday.hospitalmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.uday.hospitalmanagement.repository")
+@EntityScan(basePackages = "com.uday.hospitalmanagement.entity")
 public class HospitalManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplication.class, args);
+    }
 }
--- src/main/java/com/uday/hospitalmanagement/repository/PatientRepository.java
+++ src/main/java/com/uday/hospitalmanagement/repository/PatientRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.uday.hospitalmanagement.entity.Patient;
 
-@Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
